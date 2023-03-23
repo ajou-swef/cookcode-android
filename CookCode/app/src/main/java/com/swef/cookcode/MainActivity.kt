f2441c959ab1385312c488b8a1fa1340a5ba031a
Haein Park <kairu0404@ajou.ac.kr>
2023-03-23 14:02:24 +0900

    feat : 어플리케이션 아이콘 추가

--- CookCode/app/src/main/java/com/swef/cookcode/MainActivity.kt
+++ CookCode/app/src/main/java/com/swef/cookcode/MainActivity.kt
@@ -2,10 +2,16 @@ package com.swef.cookcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.swef.cookcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /** ViewBinding을 쓰기 위한 binding 변수 선언 : lateinit을 통해 추후 초기화 하도록 설정 **/
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
