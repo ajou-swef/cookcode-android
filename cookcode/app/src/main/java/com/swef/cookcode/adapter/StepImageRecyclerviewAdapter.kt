1022db5f79f13cee059b62a7a9dc278ee14d6c10
haeiny-cloud <kairu0404@ajou.ac.kr>
2023-04-30 20:50:45 +0900

    fix : 스텝 추가/수정 UI 이미지 삭제 오류 수정

--- cookcode/app/src/main/java/com/swef/cookcode/adapter/StepImageRecyclerviewAdapter.kt
+++ cookcode/app/src/main/java/com/swef/cookcode/adapter/StepImageRecyclerviewAdapter.kt
@@ -10,7 +10,6 @@ import com.swef.cookcode.R
 import com.swef.cookcode.data.StepImageData
 import com.swef.cookcode.databinding.StepImageRecyclerviewItemBinding
 
-
 // Step에 들어갈 Image를 보여줄 RecyclerView를 위한 adapter
 class StepImageRecyclerviewAdapter(
     // 갤러리에서 이미지를 선택할 수 있는 launcher는 activity에서 구현하여 어댑터에 넘겨준다
@@ -42,19 +41,19 @@ class StepImageRecyclerviewAdapter(
 
     // 이 명령어를 통해 recyclerview에 data를 넣어준다
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(datas[position])
+        holder.bind(datas[position], position)
     }
 
     // bind 함수에 data를 넘겨주는 함수
     inner class ViewHolder(
         private val binding: StepImageRecyclerviewItemBinding
     ) : RecyclerView.ViewHolder(binding.root) {
-        fun bind(item: StepImageData) {
+        fun bind(item: StepImageData, position: Int) {
             // 이미지 클릭 시 갤러리에서 이미지 불러올 수 있는 클릭 리스너
             binding.image.setOnClickListener {
                 if(item.imageUri != null) {
                     // 이미 이미지가 등록된 상태면 팝업 메뉴를 통해 추가, 삭제 할 수 있음
-                    showPopupMenu(binding.image, item)
+                    showPopupMenu(binding.image, item, position)
                 }
                 else {
                     pickImageLauncher.launch("*/image")
@@ -73,23 +72,31 @@ class StepImageRecyclerviewAdapter(
             }
         }
 
-        // 이미지 추가 삭제 팝업 메뉴
-        private fun showPopupMenu(view: View, item: StepImageData) {
+        // 이미지 수정 삭제 팝업 메뉴
+        private fun showPopupMenu(view: View, item: StepImageData, position: Int) {
             val popupMenu = PopupMenu(view.context, view)
             popupMenu.menuInflater.inflate(R.menu.image_popup_menu, popupMenu.menu)
 
             popupMenu.setOnMenuItemClickListener { menuItem ->
                 when (menuItem.itemId) {
-                    // 이미지 추가 버튼 클릭 시 이미지 불러오기
+                    // 이미지 수정 버튼 클릭 시 이미지 불러오기
                     R.id.update_image -> {
+                        item.imageUri = null
                         pickImageLauncher.launch("*/image")
                         true
                     }
-                    // 이미지 삭제 버튼 클릭 시 imageUri 삭제 및 기본 이미지로 변경
+                    // 이미지 삭제 버튼 클릭 시 해당 이미지 삭제 및 순서 당기기
                     R.id.delete_image -> {
                         item.imageUri = null
-                        binding.image.setImageURI(null)
-                        binding.image.setBackgroundResource(item.basicImage)
+                        for(i :Int in position until datas.size - 1) {
+                            // 해당 위치보다 뒤에 image가 있다면 앞으로 당겨오기
+                            if (datas[i + 1].imageUri != null) {
+                                datas[i].imageUri = datas[i + 1].imageUri
+                                datas[i + 1].imageUri = null
+                            }
+                        }
+
+                        notifyDataSetChanged()
                         true
                     }
                     else -> false
